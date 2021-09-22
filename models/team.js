caaee0481b5d5e41f22545693ba3af3e63d01698
thatFemicode <leyedavid10@gmail.com>
2021-09-22 20:30:23 +0100

    fixed searching

--- models/team.js
+++ models/team.js
@@ -17,5 +17,33 @@ const teamSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  coverImage: {
+    type: Buffer,
+    // required: true,
+  },
+  coverImageType: {
+    type: String,
+    // required: true,
+  },
+});
+// pre allows us to run a method before actions
+// we are trying to create something where we cant delete a team when it has players
+teamSchema.pre('remove', function (next) {
+  Player.find({ team: this.id }, (err, player) => {
+    if (err) {
+      next(err);
+    } else if (player.length > 0) {
+      next(new Error('This Team has players'));
+    } else {
+      next();
+    }
+  });
+});
+teamSchema.virtual('coverImagePath').get(function () {
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${
+      this.coverImageType
+    };charset=utf-8;base64,${this.coverImage.toString('base64')}`;
+  }
 });
 module.exports = mongoose.model('Team', teamSchema);
--- routes/teams.js
+++ routes/teams.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const Team = require('../models/team');
+const Player = require('../models/player');
+const imageMimeTypes = [
+  'image/jpeg',
+  'image/png',
+  'images/gif',
+  'image/svg+xml',
+];
 
 // All TEAM ROUTE
 router.get('/', async (req, res) => {
@@ -11,6 +18,7 @@ router.get('/', async (req, res) => {
   try {
     const teams = await Team.find(searchOptions);
     res.render('teams/index', { teams: teams, searchOptions: req.query });
+    // console.log(teams, cover);
   } catch {
     res.redirect('/');
   }
@@ -28,10 +36,13 @@ router.post('/', async (req, res) => {
     arena: req.body.arena,
     tag: req.body.tag,
   });
+  saveCover(team, req.body.cover);
+
   try {
     // this is to save the teams in the databse
     const newTeam = await team.save();
-    res.redirect('teams');
+    // res.redirect('teams');
+    res.redirect(`teams/${newTeam.id}`);
   } catch (err) {
     res.render('teams/new', {
       team: team,
@@ -49,4 +60,80 @@ router.post('/', async (req, res) => {
   //   }
   // });
 });
+router.get('/:id', async (req, res) => {
+  // console.log(req.params.id);
+  try {
+    const team = await Team.findById(req.params.id);
+    const players = await Player.find({ team: team.id }).limit(7).exec();
+    res.render('teams/show', {
+      team: team,
+      playersInTeam: players,
+    });
+  } catch (err) {
+    console.log(err);
+    res.redirect('/');
+  }
+});
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const team = await Team.findById(req.params.id);
+    res.render('teams/edit', { team });
+  } catch {
+    res.redirect('teams');
+  }
+});
+router.put('/:id', async (req, res) => {
+  // team variable must be available inside try and catch method
+  let team;
+  // saveCover(team, req.body.cover);
+  try {
+    team = await Team.findById(req.params.id);
+    team.teamName = req.body.teamName;
+    team.location = req.body.location;
+    team.arena = req.body.arena;
+    team.tag = req.body.tag;
+    await team.save();
+    res.redirect(`/teams/${team.id}`);
+    // this is to save the teams in the databse
+  } catch (err) {
+    console.log(err);
+    if (team == null) {
+      res.redirect('/');
+    } else {
+      res.render('teams/edit', {
+        team: team,
+        errorMessage: 'Error Updating Team',
+      });
+    }
+  }
+});
+router.delete('/:id', async (req, res) => {
+  // team variable must be available inside try and catch method
+  let team;
+  // saveCover(team, req.body.cover);
+  try {
+    team = await Team.findById(req.params.id);
+    await team.remove();
+    res.redirect(`/teams/${team.id}`);
+    // this is to save the teams in the databse
+  } catch (err) {
+    console.log(err);
+    if (team == null) {
+      res.redirect('/');
+    } else {
+      res.redirect(`/teams/${team.id}`);
+    }
+  }
+});
+
+function saveCover(player, coverEncoded) {
+  if (coverEncoded == null) return;
+  const cover = JSON.parse(coverEncoded);
+  if (cover != null && imageMimeTypes.includes(cover.type)) {
+    // data string cant be sent like that it has to be converted to a buffer
+    player.coverImage = new Buffer.from(cover.data, 'base64');
+    // this way we can extract this buffer and convert to an image on the correct type
+    player.coverImageType = cover.type;
+  }
+}
 module.exports = router;
